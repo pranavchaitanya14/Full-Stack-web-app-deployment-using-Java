66aade09080cd79197d2fb822ec5b71aadd8d69f
P. Pranav chaitanya <pranavchaitanya14@gmail.com>
2026-04-16 17:31:33 +0530

    Update login.js

--- app/login.js
+++ app/login.js
@@ -7,7 +7,7 @@ export default function Login() {
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
-    await axios.post("/api/login", { email, passwOrd });
+    await axios.post("/api/login", { email, password });
     window.location.href = "/dashboard";
   };
 
